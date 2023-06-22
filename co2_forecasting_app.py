24016c8e0e98e42686b1ed68ad42a3257e9db3e4
Geram4305 <newstar4305@gmail.com>
2023-06-22 15:58:13 -0400

    updates

--- co2_forecasting_app.py
+++ co2_forecasting_app.py
@@ -12,7 +12,7 @@ warnings.filterwarnings("ignore")
 
 
 #load the model
-model = pickle.load(open('/Users/geet/Documents/Projects_Git/EcoTrendPredictor/C02_forecast_Model.pickle','rb'))
+model = pickle.load(open('/Users/geet/Documents/Projects_Git/EcoTrendPredictor/CO2_forecast_Model.pkl','rb'))
 
 #load dataset to plot alongside predictions
 df = pd.read_csv("/Users/geet/Documents/Projects_Git/EcoTrendPredictor/Data/CO2.csv")
@@ -21,15 +21,16 @@ df.set_index(['Year'], inplace=True)
 
 
 #page configuration
-st.set_page_config(layout='centered')
+st.set_page_config(layout='centered') #configures the page layout to be centered
 image = Image.open('/Users/geet/Downloads/co2.jpg')
-st.image(image)
+st.image(image) # displays the image on the Streamlit application
 
-year = st.slider("Select number of Years",1,30,step = 1)
+year = st.slider("Select number of Years",1,30,step = 1) #creates a slider widget where the user can select a 
+                                                        #number of years between 1 and 30.
     
     
-pred = model.forecast(len(year))
-pred = pd.DataFrame(pred, columns=['CO2'])
+pred = model.forecast(year) #predict future values based on the selected number of years.
+pred = pd.DataFrame(pred, columns=['CO2'])  # predicted values are stored in pred.
    
 if st.button("Predict"):
 
@@ -37,7 +38,14 @@ if st.button("Predict"):
         with col1:
              st.dataframe(pred)
         with col2:
-            fig, ax = plt.subplots()
+            fig, ax = plt.subplots(figsize=(10, 12))
             df['CO2'].plot(style='--', color='gray', legend=True, label='known')
             pred['CO2'].plot(color='b', legend=True, label='prediction')
+            
+            # Increase font size
+            plt.xlabel('X-axis label', fontsize=12)
+            plt.ylabel('Y-axis label', fontsize=12)
+            plt.xticks(fontsize=12)
+            plt.yticks(fontsize=12)
+            plt.legend(fontsize=12)
             st.pyplot(fig)
